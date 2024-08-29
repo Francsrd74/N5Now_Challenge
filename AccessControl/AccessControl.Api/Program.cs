05df0baa66d968f187e02e67e9bf2182ead82199
Francisco Fernández <francisco.fernandez@asociart.com.ar>
2024-08-29 18:21:53 -0300

    Add Startup

--- AccessControl/AccessControl.Api/Program.cs
+++ AccessControl/AccessControl.Api/Program.cs
@@ -1,25 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+using AccessControl.Api;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        //var configuration = new ConfigurationBuilder()
+        //    .AddJsonFile("appsettings.json")
+        //    .Build(); 
+
+        try
+        { 
+            CreateHostBuilder(args).Build().Run();
+        }
+        catch (Exception ex)
+        { 
+        } 
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<StartUp>(); 
+            });
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ AccessControl/AccessControl.Api/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using AccessControl.Infrastructure;
+
+namespace AccessControl.Api
+{
+    public class StartUp
+    {
+        public StartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            //add Infra
+            services.AddInfrastructure(Configuration); 
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+                // Configure the HTTP request pipeline.
+                if (env.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+             
+            
+        }
+    }
+}
