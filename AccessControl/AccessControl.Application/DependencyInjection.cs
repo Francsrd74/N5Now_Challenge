895612cc17268df47c010d1ba39640b4cdea09c9
Francisco Fernández <francisco.fernandez@asociart.com.ar>
2024-08-30 20:50:19 -0300

    add DependenciaInjection for application assembly

---
+++ AccessControl/AccessControl.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessControl.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+
+            //add mediator for this assembly
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+
+            return services;    
+        }
+    }
+}
