4daef2c46e6ddd2914c223cc699c806b2fd366bb
Francisco Fernández <francisco.fernandez@asociart.com.ar>
2024-08-30 22:04:58 -0300

    Add Validator to GetPermissionRequest

---
+++ AccessControl/AccessControl.Application/Permissions/Queries/GetPermission/GetPermissionRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using AccessControl.Application.Common.Interfaces;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessControl.Application.Permissions.Queries.GetPermission
+{
+    public class GetPermissionRequestValidator : AbstractValidator<GetPermissionRequest>
+    {
+        public GetPermissionRequestValidator(IAccessControlContext _context)
+        {
+            RuleFor(r => r.Id).NotEmpty().NotNull().WithMessage("The field is empty or null");
+                
+            RuleFor(r => r.Id).Must(m =>
+            {  
+                var permission = _context.Permissions.AsNoTracking().FirstOrDefault(f => f.Id.Equals(m));
+
+                return permission != null;
+
+            }).WithMessage("Permission Not Found");  
+             
+        }
+    }
+
+}
